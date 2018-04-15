70b4b98c81decb7cef1e2aa2b85009f97302b982
Michael Evans <michael.j.evans@vanderbilt.edu>
2018-04-15 18:47:00 -0500

    Added independent audio

--- Client.py
+++ Client.py
@@ -1,13 +1,24 @@
 import cv2
 import numpy as np
 import time
-#import pyaudio
+import pyaudio
 import socket
+import wave
+import pickle
+import json
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 print("connecting to localhost")
 client_socket.connect(('localhost', 8080))
+
+wf = wave.open("Man's Not Hot.wav")
+p = pyaudio.PyAudio()
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
 
 try:
 
@@ -15,25 +26,32 @@ try:
 
     while data:
         #print("receiving data")
-        data = client_socket.recv(500000)
+        #data = client_socket.recv(500000)
 
-        nparr = np.fromstring(data, np.uint8)
+        #print(data.decode('utf-8'))
+        #data = json.loads(data.decode('utf-8'))
+        audio = client_socket.recv(500000)
 
-        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        #nparr = np.fromstring(data.get("frame"), np.uint8)
 
 
-        if type(frame) is type(None):
-            pass
-        else:
-            try:
-                cv2.imshow('frame', frame)
-                if cv2.waitKey(25) == ord('q'):
-                    client_socket.close()
-                    exit(0)
+        #frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-            except:
+
+        #if type(frame) is type(None):
+            #pass
+        #else:
+        try:
+            #cv2.imshow('frame', frame)
+            stream.write(audio)
+            if cv2.waitKey(25) == ord('q'):
                 client_socket.close()
                 exit(0)
+
+        except:
+            client_socket.close()
+            exit(0)
 
 
 
--- Server.py
+++ Server.py
@@ -2,6 +2,13 @@ import sys
 import socket
 import cv2
 import time
+import pyaudio
+import wave
+import numpy
+import pickle
+import json
+
+CHUNK = 1024
 
 cap = cv2.VideoCapture("Man's Not Hot.mp4")
 
@@ -11,7 +18,18 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind(serv_address)
 
+wf = wave.open("Man's Not Hot.wav")
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+
 sock.listen(1)
+
 
 while True:
     print("Waiting to connect")
@@ -23,10 +41,19 @@ while True:
 
         while (True):
             ret, frame = cap.read()
+            audio = wf.readframes(CHUNK)
 
-            data = cv2.imencode('.jpg', frame)[1].tostring()
+            frame_data = str(cv2.imencode('.jpg', frame)[1])
 
-            new_connection.send(data)
+
+            #data = json.dumps((cv2.imencode('.jpg', frame)[1].tostring(), audio))
+
+            #data = json.dumps({"frame": frame_data, "audio": str(audio)})
+            #print(data)
+            #print(len(data))
+            #new_connection.send(frame_data)
+
+            new_connection.send(audio)
             #cv2.imshow('frame', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
