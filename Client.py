3aaf7868daf4b831db895150edff3475a7976804
Michael Evans <michael.j.evans@vanderbilt.edu>
2018-04-27 01:14:17 -0500

    Implemented threaded buffering.

--- Client.py
+++ Client.py
@@ -6,56 +6,100 @@ import socket
 import wave
 import pickle
 import json
+import threading
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+vidBuffer = []
 
-print("connecting to localhost")
-client_socket.connect(('localhost', 8080))
 
-wf = wave.open("Man's Not Hot.wav")
-p = pyaudio.PyAudio()
-
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
-try:
-
+def getData():
+    print("connecting to localhost")
+    client_socket.connect(('localhost', 8080))
     data = client_socket.recv(1024)
+    try:
+        while data:
+            data = client_socket.recv(500000)
+            vidBuffer.append(data)
 
-    while data:
-        #print("receiving data")
-        data = client_socket.recv(500000)
+        print("whole video loaded")
 
-        #print(data.decode('utf-8'))
-        #data = json.loads(data.decode('utf-8'))
-        #audio = client_socket.recv(500000)
+    finally:
+        print("closing socket")
+        client_socket.close()
 
+def main():
 
-        nparr = np.fromstring(data, np.uint8)
+    wf = wave.open("Man's Not Hot.wav")
+    p = pyaudio.PyAudio()
 
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    threading.Thread(target=getData, args=[]).start()
+    while len(vidBuffer) < 20:
+        print(len(vidBuffer))
+
+    index = 0
+    while True:
+        if index == len(vidBuffer):
+            time.sleep(2)
+        nparr = np.fromstring(vidBuffer[index], np.uint8)
 
         frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
 
         if type(frame) is type(None):
             pass
         else:
             try:
                 cv2.imshow('frame', frame)
-                #stream.write(audio)
-                if cv2.waitKey(25) == ord('q'):
+                if cv2.waitKey(41) == ord('q'):
                     client_socket.close()
                     exit(0)
-
             except:
                 client_socket.close()
                 exit(0)
+        index += 1
+    #data = client_socket.recv(1024)
+
+    # try:
+            #while data:
+            #print("receiving data")
+            #data = client_socket.recv(500000)
+
+            #print(data.decode('utf-8'))
+            #data = json.loads(data.decode('utf-8'))
+            #audio = client_socket.recv(500000)
+
+
+            #nparr = np.fromstring(data, np.uint8)
+
+
+            #frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+
+    #         if type(frame) is type(None):
+    #             pass
+    #         else:
+    #             try:
+    #                 cv2.imshow('frame', frame)
+    #                 #stream.write(audio)
+    #                 if cv2.waitKey(25) == ord('q'):
+    #                     client_socket.close()
+    #                     exit(0)
+    #
+    #             except:
+    #                 client_socket.close()
+    #                 exit(0)
+    #
+    #
+    #
+    #
+    # finally:
+    #     print("closing socket")
+    #     client_socket.close()
 
 
 
-
-finally:
-    print("closing socket")
-    client_socket.close()
+main()
--- Server.py
+++ Server.py
@@ -7,62 +7,66 @@ import wave
 import numpy
 import pickle
 import json
-
-CHUNK = 1024
-
-cap = cv2.VideoCapture("Man's Not Hot.mp4")
-
-serv_address = ('localhost', 8080)
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-sock.bind(serv_address)
-
-wf = wave.open("Man's Not Hot.wav")
-
-p = pyaudio.PyAudio()
-
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
+import threading
 
 
-sock.listen(1)
+def main():
+    CHUNK = 1024
+
+    cap = cv2.VideoCapture("Man's Not Hot.mp4")
+
+    serv_address = ('localhost', 8080)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    sock.bind(serv_address)
+
+    wf = wave.open("Man's Not Hot.wav")
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
 
 
-while True:
-    print("Waiting to connect")
-
-    new_connection, client_address = sock.accept()
-
-    try:
-        print('connection from', client_address)
-
-        while (True):
-            ret, frame = cap.read()
-            audio = wf.readframes(CHUNK)
-
-            frame_data = cv2.imencode('.jpg', frame)[1]
+    sock.listen(1)
 
 
-            #data = json.dumps((cv2.imencode('.jpg', frame)[1].tostring(), audio))
+    while True:
+        print("Waiting to connect")
 
-            #data = json.dumps({"frame": frame_data, "audio": str(audio)})
-            #print(data)
-            #print(len(data))
-            new_connection.send(frame_data)
+        new_connection, client_address = sock.accept()
 
-            #new_connection.send(audio)
-            #cv2.imshow('frame', frame)
+        try:
+            print('connection from', client_address)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            while (True):
+                ret, frame = cap.read()
+                audio = wf.readframes(CHUNK)
 
-    finally:
-        cap.release()
-        cv2.destroyAllWindows()
-        print("Closing connection with client. \n")
-        new_connection.close()
+                frame_data = cv2.imencode('.jpg', frame)[1]
 
 
+                #data = json.dumps((cv2.imencode('.jpg', frame)[1].tostring(), audio))
+
+                #data = json.dumps({"frame": frame_data, "audio": str(audio)})
+                #print(data)
+                #print(len(data))
+                new_connection.send(frame_data)
+
+                #new_connection.send(audio)
+                #cv2.imshow('frame', frame)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+        finally:
+            cap.release()
+            cv2.destroyAllWindows()
+            print("Closing connection with client. \n")
+            new_connection.close()
+
+
+main()
